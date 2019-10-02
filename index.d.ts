57df0defa1e72a3918b74966bff51ed5a20530ee
akim95 <m.akmalhakim95@gmail.com>
2019-10-02 11:13:00 +0800

    improvement for ts defintiions

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,11 @@
 // TypeScript Version: 3.5.2
 
-export declare function createWriter<T>(redis: T, key: string): T;
-export declare function createReader<T>(redis: T, key: string, opt?: T): T;
+interface Options {
+  live: boolean,
+  waitTimeout: number,
+  history: boolean,
+  limit: number,
+}
+
+export declare function createWriter<T>(redis: T, key: string): NodeJS.WritableStream;
+export declare function createReader<T>(redis: T, key: string, opt?: Options | T): NodeJS.ReadableStream;
